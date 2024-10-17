e26c0d5d316c483b781eeaa2bbefbd7e6fa9a5f9
Theo Saveliev <89431871+theosaveliev@users.noreply.github.com>
2024-10-17 06:30:19 +0500

    Revert b8291c. Power adjustment.
    3015 0.01 0.06 0.32
    1846 0.04 0.32 1.24

--- ui/menu.c
+++ ui/menu.c
@@ -276,14 +276,14 @@ const char gSubMenu_RESET[][4] =
 
 const char * const gSubMenu_F_LOCK[] =
 {
-	"DISABLED",
-	"DISABLED",
-	"DISABLED",
-	"DISABLED",
-	"DISABLED",
+	"DEFAULT+\n137-174\n400-470",
+	"FCC HAM\n144-148\n420-450",
+	"CE HAM\n144-146\n430-440",
+	"GB HAM\n144-148\n430-440",
+	"137-174\n400-430",
 	"PMR",
 	"DISABLE\nALL",
-	"DISABLED",
+	"UNLOCK\nALL",
 };
 
 const char gSubMenu_BACKLIGHT[][7] =
